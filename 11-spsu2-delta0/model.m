5f45ea9a615c21178e7688ded173d815dbeb94c9
donrolih <don.rolih@gmail.com>
2023-09-07 13:19:32 +0200

    Fixed off diagonal elements of Hartree term

--- 11-spsu2-delta0/model.m
+++ 11-spsu2-delta0/model.m
@@ -9,13 +9,16 @@ Hselfd = H1;
 selfopd =  ( Chop @ Expand @ komutator[Hselfd /. params, d[#1, #2]] )&;
 selfopcd = ( Chop @ Expand @ komutator[Hselfd /. params, ((-1)^#2 d[1-#1, 1-#2])] )&;
 
+(* selfopd =  ( Chop @ Expand @ komutator[ d[#1, #2], Hselfd /. params] )&;
+selfopcd = ( Chop @ Expand @ komutator[((-1)^#2 d[1-#1, 1-#2]), Hselfd /. params] )&; *)
+
 (* This is the constant Hartree term needed for the self-energy *)
 SigmaHd = Expand @ antikomutator[ selfopd[#1, #2], d[#3, #4] ] /. params &;
-
 SigmaHdAvg11 := Expand @ (SigmaHd[CR, UP, AN, UP]+SigmaHd[CR, DO, AN, DO])/2;
-SigmaHdAvg12 := Expand @ (SigmaHd[AN, UP, AN, UP]+SigmaHd[AN, DO, AN, DO])/2;
-SigmaHdAvg21 := Expand @ (SigmaHd[CR, DO, CR, DO]+SigmaHd[CR, UP, CR, UP])/2;
+SigmaHdAvg12 := Expand @ (SigmaHd[AN, DO, AN, UP]+SigmaHd[AN, UP, AN, DO])/2;
+SigmaHdAvg21 := Expand @ (SigmaHd[CR, UP, CR, DO]+SigmaHd[CR, DO, CR, UP])/2;
 SigmaHdAvg22 := Expand @ (SigmaHd[AN, DO, CR, DO]+SigmaHd[AN, UP, CR, UP])/2;
+
 
 Print["SigmaH[AN, UP, CR, UP]=", SigmaHd[CR, UP, AN, UP] ];
 Print["SigmaH[AN, UP, AN, DO]=", SigmaHd[AN, DO, AN, UP] ];
